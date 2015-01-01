68711bf6e6167ece7f07b95ee5c39cd20dfcf33d
Micah <micah@zoltu.net>
2015-01-01 13:57:21 -0800

    Creates MillisecondTimeSpanJsonConverter and tests.

---
+++ Code/MillisecondTimeSpanJsonConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Zoltu.JsonNet.Converters
+{
+	public class MillisecondTimeSpanJsonConverter : JsonConverter
+	{
+		public override Boolean CanConvert(Type objectType)
+		{
+			if (objectType == typeof(TimeSpan))
+				return true;
+			else
+				return false;
+		}
+
+		public override Object ReadJson(JsonReader reader, Type objectType, Object existingValue, JsonSerializer serializer)
+		{
+			var jToken = JToken.Load(reader);
+			return TimeSpan.FromMilliseconds((Double)jToken.ToObject<Int64>());
+		}
+
+		public override void WriteJson(JsonWriter writer, Object value, JsonSerializer serializer)
+		{
+			var timeSpan = (TimeSpan)value;
+			writer.WriteValue((Int64)timeSpan.TotalMilliseconds);
+		}
+	}
+}
---
+++ Tests/MillisecondTimeSpanJsonConverterTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Newtonsoft.Json;
+using Xunit;
+using Zoltu.JsonNet.Converters;
+
+namespace Tests
+{
+	public class MillisecondTimeSpanJsonConverterTests
+	{
+		[Fact]
+		public void serialize_timespan()
+		{
+			const String expected = @"""1.02:03:04.0050000""";
+			var model = new TimeSpan(1, 2, 3, 4, 5);
+
+			var actual = JsonConvert.SerializeObject(model);
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void deserialize_timespan()
+		{
+			const String json = @"""1.02:03:04.005""";
+			var expected = new TimeSpan(1, 2, 3, 4, 5);
+
+			var actual = JsonConvert.DeserializeObject<TimeSpan>(json);
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void serialize_timespan_as_milliseconds()
+		{
+			const String expected = @"5000";
+			var inputTimeSpan = TimeSpan.FromMilliseconds(5000);
+
+			var actual = JsonConvert.SerializeObject(inputTimeSpan, new MillisecondTimeSpanJsonConverter());
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void deserialize_timespan_as_milliseconds()
+		{
+			var expected = TimeSpan.FromMilliseconds(5000);
+			const String inputJson = @"5000";
+
+			var actual = JsonConvert.DeserializeObject<TimeSpan>(inputJson, new MillisecondTimeSpanJsonConverter());
+
+			Assert.Equal(expected, actual);
+		}
+	}
+}
