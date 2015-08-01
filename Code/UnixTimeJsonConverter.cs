4744a98d14920c16131552705fe83463650de17f
Micah Zoltu <micah@zoltu.net>
2015-08-01 14:51:03 -0700

    Adds converter for unix time.

--- Code/UnixTimeJsonConverter.cs
+++ Code/UnixTimeJsonConverter.cs
@@ -5,11 +5,13 @@ using Newtonsoft.Json.Linq;
 
 namespace Zoltu.JsonNet.Converters
 {
-	public class MillisecondTimeSpanJsonConverter : JsonConverter
+	public class UnixTimeJsonConverter : JsonConverter
 	{
+		private static readonly DateTimeOffset UnixEpoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
 		public override Boolean CanConvert(Type objectType)
 		{
-			if (objectType == typeof(TimeSpan))
+			if (objectType == typeof(DateTimeOffset))
 				return true;
 			else
 				return false;
@@ -19,15 +21,17 @@ namespace Zoltu.JsonNet.Converters
 		{
 			var jToken = JToken.Load(reader);
 			Contract.Assume(jToken != null);
-			return TimeSpan.FromMilliseconds((Double)jToken.ToObject<Int64>());
+			var secondsSinceEpoch = TimeSpan.FromSeconds(jToken.ToObject<Int64>());
+			return UnixEpoch + secondsSinceEpoch;
 		}
 
 		public override void WriteJson(JsonWriter writer, Object value, JsonSerializer serializer)
 		{
 			Contract.Assume(writer != null);
 			Contract.Assume(value != null);
-			var timeSpan = (TimeSpan)value;
-			writer.WriteValue((Int64)timeSpan.TotalMilliseconds);
+			var dateTime = (DateTimeOffset)value;
+			var timeSinceEpoch = dateTime - UnixEpoch;
+			writer.WriteValue((Int64)timeSinceEpoch.TotalSeconds);
 		}
 	}
 }
---
+++ Tests/UnixTimeJsonConverterTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Newtonsoft.Json;
+using Xunit;
+using Zoltu.JsonNet.Converters;
+
+namespace Tests
+{
+	public class UnixTimeJsonConverterTests
+	{
+		[Fact]
+		public void serialize_epoch_with_built_in_serializer()
+		{
+			const String expected = @"""1980-01-01T00:00:00+00:00""";
+			var model = new DateTimeOffset(1980, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+			var actual = JsonConvert.SerializeObject(model);
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void deserialize_epoch_with_built_in_serializer()
+		{
+			const String json = @"""0001-01-01T00:00:00+00:00""";
+			var expected = new DateTimeOffset(1, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+			var actual = JsonConvert.DeserializeObject<DateTimeOffset>(json);
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void serialize_epoch_as_unixtime()
+		{
+			const String expected = @"0";
+			var model = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+			var actual = JsonConvert.SerializeObject(model, new UnixTimeJsonConverter());
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void deserialize_epoch_as_unixtime()
+		{
+			var expected = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+			const String inputJson = @"0";
+
+			var actual = JsonConvert.DeserializeObject<DateTimeOffset>(inputJson, new UnixTimeJsonConverter());
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void serialize_nowish_as_unixtime()
+		{
+			const String expected = @"1438434855";
+			var model = new DateTimeOffset(2015, 08, 1, 13, 14, 15, TimeSpan.Zero);
+
+			var actual = JsonConvert.SerializeObject(model, new UnixTimeJsonConverter());
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void deserialize_nowish_as_unixtime()
+		{
+			var expected = new DateTimeOffset(2015, 08, 1, 13, 14, 15, TimeSpan.Zero);
+			const String inputJson = @"1438434855";
+
+			var actual = JsonConvert.DeserializeObject<DateTimeOffset>(inputJson, new UnixTimeJsonConverter());
+
+			Assert.Equal(expected, actual);
+		}
+	}
+}
